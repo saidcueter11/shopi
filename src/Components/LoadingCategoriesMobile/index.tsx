e004a80e921d0571e219a44f7664129dd1a1a27a
Said Cueter <rattion@pop-os.localdomain>
2023-08-14 23:50:11 -0400

    Added loading to categoreis in mobile

---
+++ src/Components/LoadingCategoriesMobile/index.tsx
@@ -0,0 +1,12 @@
+export const LoadingCategoriesMobile = () => {
+  return (
+    <div className="flex gap-2">
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+      <div className="w-28 h-7 rounded-full bg-slate-100 animate-pulse"></div>
+    </div>
+  )
+}
